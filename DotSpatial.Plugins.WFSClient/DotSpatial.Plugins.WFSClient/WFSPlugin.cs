34e9914397f4c157b3fd4dbcf2656163ac9b1c20
carosoisu_cp <SND\carosoisu_cp>
2012-04-13 17:29:55 -0700

    WFS Client plugin

---
+++ DotSpatial.Plugins.WFSClient/DotSpatial.Plugins.WFSClient/WFSPlugin.cs
@@ -0,0 +1,38 @@
+﻿namespace DotSpatial.Plugins.WFSClient
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using DotSpatial.Controls;
+    using DotSpatial.Controls.Header;
+    using WFSPlugin;
+    public class MyPlugin1 : Extension
+    {
+        private const string FileMenuKey = "WFS Client";
+        // private const string HomeMenuKey = HeaderControl.HomeRootItemKey;
+
+        public override void Activate()
+        {
+            App.HeaderControl.Add(new RootItem(FileMenuKey, "WFS Client") { SortOrder = -20 });
+            // App.HeaderControl.Add(new SimpleActionItem(HeaderControl.ApplicationMenuKey, "About", ButtonClick) { GroupCaption = HeaderControl.ApplicationMenuKey, SortOrder = 500 });
+            IHeaderControl header = App.HeaderControl;
+            header.Add(new SimpleActionItem(FileMenuKey, "WFS", ButtonClick) { GroupCaption = HeaderControl.ApplicationMenuKey, SortOrder = 5 });
+
+            base.Activate();
+        }
+
+        public override void Deactivate()
+        {
+            if (App.HeaderControl != null) { App.HeaderControl.RemoveAll(); }
+            base.Deactivate();
+        }
+
+        public void ButtonClick(object sender, EventArgs e)
+        {
+            var form = new WFSServerParameters();
+            form.map = App.Map as Map;
+            form.Show();
+        }
+    }
+}
