198a5561ac83f547b5e2794f6d158e2e64ee8970
mudnug_cp <SND\mudnug_cp>
2012-04-20 11:25:39 -0700

    Add: MockICancelProgressHandler for functions like GetSlope

---
+++ DotSpatial.Data/MockICancelProgressHandler.cs
@@ -0,0 +1,36 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="WrapIProgressHandler.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace DotSpatial.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// This class converts an IProgressHandler to an ICancelProgressHandler. It wraps the IProgressHandler so that
+    /// it can be used by a class that requires an ICancelProgressHandler. Cancel will always return false.
+    /// </summary>
+    public class MockICancelProgressHandler : ICancelProgressHandler
+    {
+        private IProgressHandler _ProgressHandler;
+        public MockICancelProgressHandler(IProgressHandler progressHandler)
+        {
+            _ProgressHandler = progressHandler;
+        }
+
+        public bool Cancel
+        {
+            get { return false; }
+        }
+
+        public void Progress(string key, int percent, string message)
+        {
+            _ProgressHandler.Progress(key, percent, message);
+        }
+    }
+}
