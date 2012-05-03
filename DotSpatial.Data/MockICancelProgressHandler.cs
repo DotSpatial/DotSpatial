7982a65a6bc307662c80196b789b2a0ec148064b
mudnug_cp <SND\mudnug_cp>
2012-05-03 15:21:15 -0700

    Add: missing XML documentation

--- DotSpatial.Data/MockICancelProgressHandler.cs
+++ DotSpatial.Data/MockICancelProgressHandler.cs
@@ -18,16 +18,30 @@ namespace DotSpatial.Data
     public class MockICancelProgressHandler : ICancelProgressHandler
     {
         private IProgressHandler _ProgressHandler;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MockICancelProgressHandler"/> class.
+        /// </summary>
+        /// <param name="progressHandler">The progress handler.</param>
         public MockICancelProgressHandler(IProgressHandler progressHandler)
         {
             _ProgressHandler = progressHandler;
         }
 
+        /// <summary>
+        /// Returns true if the running process should be cancelled
+        /// </summary>
         public bool Cancel
         {
             get { return false; }
         }
 
+        /// <summary>
+        /// Progress is the method that should receive a progress message.
+        /// </summary>
+        /// <param name="key">The message string without any information about the status of completion.</param>
+        /// <param name="percent">An integer from 0 to 100 that indicates the condition for a status bar etc.</param>
+        /// <param name="message">A string containing both information on what the process is, as well as its completion status.</param>
         public void Progress(string key, int percent, string message)
         {
             _ProgressHandler.Progress(key, percent, message);
