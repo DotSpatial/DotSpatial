9b0639335981c57c60c825c382b21951fb2de1e6
danames_cp <SND\danames_cp>
2013-03-05 12:57:50 -0800

    Streamlining the buffer tool as an model example of how we want to build up our tool library - i.e. with simple funtional tool wrappers around analytical and data management functions. Users should generally use the direct function (i.e. Feature.Buffer) in code. This tool object is primarily for use in the toolbox.

--- DotSpatial.Tools/Buffer.cs
+++ DotSpatial.Tools/Buffer.cs
@@ -1,27 +1,17 @@
-﻿// ********************************************************************************************************
-// Product Name: MapWindow.Tools.Buffer
-// Description:  Inverse Distance Weighting point to raster interpolation
-//
-// ********************************************************************************************************
-// The contents of this file are subject to the Mozilla Public License Version 1.1 (the "License");
-// you may not use this file except in compliance with the License. You may obtain a copy of the License at
-// http://www.mozilla.org/MPL/
-//
-// Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
-// ANY KIND, either expressed or implied. See the License for the specific language governing rights and
-// limitations under the License.
-//
-// The Original Code is Toolbox.dll for the MapWindow 4.6/6 ToolManager project
-//
-// The Initializeializeial Developer of this Original Code is Kandasamy Prasanna. Created in 2009.
-//
-// Contributor(s): (Open source contributors should list themselves and their modifications here).
+﻿// *******************************************************************************************************
+// Product: DotSpatial.Tools.Buffer
+// Description:  Wraps DotSpatial.Feature.Buffer in a tool wrapper for use through the DotSptial toolbox.
+// Copyright & License: See www.DotSpatial.org.
+// Contributor(s): Open source contributors may list themselves and their modifications here.
+// Contribution of code constitutes transferral of copyright from authors to DotSpatial copyright holders. 
+//--------------------------------------------------------------------------------------------------------
 // Name               |   Date             |         Comments
-//--------------------|--------------------|--------------------------------------------------------
+//--------------------|--------------------|--------------------------------------------------------------
 // Ted Dunsford       |  8/24/2009         |  Cleaned up some unnecessary references using re-sharper
 // KP                 |  9/2009            |  Used IDW as model for Buffer
 // Ping  Yang         |  12/2009           |  Cleaning code and fixing bugs.
-// ********************************************************************************************************
+// Dan Ames           |  3/2013            |  Cleaning up header and interface as model for other tools. 
+// *******************************************************************************************************
 
 using System;
 using DotSpatial.Data;
@@ -31,22 +21,18 @@ using DotSpatial.Topology;
 namespace DotSpatial.Tools
 {
     /// <summary>
-    /// Buffer
+    /// This tool provides access to buffer functionality native to DotSpatial.Features. 
+    /// DotSpatial tools are intended to be used through the DotSpatial toolbox or modeler.
+    /// To perform buffer analysis through code, consider using DotSpatial.Feature.Buffer directly.
     /// </summary>
     public class Buffer : Tool
     {
-        #region Constants and Fields
-
+        // Declare input and output parameter arrays
         private Parameter[] _inputParam;
-
         private Parameter[] _outputParam;
 
-        #endregion
-
-        #region Constructors and Destructors
-
         /// <summary>
-        /// Creates a new instance of the buffer tool
+        /// Create a new instance of the buffer tool
         /// </summary>
         public Buffer()
         {
@@ -56,12 +42,21 @@ namespace DotSpatial.Tools
             this.ToolTip = TextStrings.Bufferwithdistance;
         }
 
-        #endregion
-
-        #region Public Properties
+        /// <summary>
+        /// Inititalize input and output arrays with parameter types and default values.
+        /// </summary>
+        public override void Initialize()
+        {
+            _inputParam = new Parameter[2];
+            _inputParam[0] = new FeatureSetParam(TextStrings.InputFeatureSet);
+            _inputParam[1] = new DoubleParam(TextStrings.BufferDistance, 10.0);
+            _outputParam = new Parameter[1];
+            _outputParam[0] = new PolygonFeatureSetParam(TextStrings.OutputPolygonFeatureSet);
+        }
 
         /// <summary>
-        /// Gets or Sets the input paramater array
+        /// Gets or Sets the input paramater array. 
+        /// Number of parameter and parameter types are defined during initialize. 
         /// </summary>
         public override Parameter[] InputParameters
         {
@@ -72,7 +67,8 @@ namespace DotSpatial.Tools
         }
 
         /// <summary>
-        /// Gets or Sets the output paramater array
+        /// Gets or Sets the input paramater array. 
+        /// Number of parameter and parameter types are defined during initialize. 
         /// </summary>
         public override Parameter[] OutputParameters
         {
@@ -82,184 +78,42 @@ namespace DotSpatial.Tools
             }
         }
 
-        #endregion
-
-        #region Public Methods
-
-        /// <summary>
-        /// Buffer test
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="bufferDistance"></param>
-        /// <param name="output"></param>
-        /// <param name="cancelProgressHandler"></param>
-        /// <returns></returns>
-        public static bool GetBuffer(
-            IFeatureSet input, double bufferDistance, IFeatureSet output, ICancelProgressHandler cancelProgressHandler)
-        {
-            int previous = 0;
-            int maxNo = input.Features.Count;
-            for (int i = 0; i < maxNo; i++)
-            {
-                input.Features[i].Buffer(bufferDistance, output);
-
-                // Here we update the progress
-                int current = Convert.ToInt32(i * 100 / maxNo);
-                if (current > previous)
-                {
-                    cancelProgressHandler.Progress(string.Empty, current, current + TextStrings.progresscompleted);
-                    previous = current;
-                }
-
-                if (cancelProgressHandler.Cancel)
-                {
-                    return false;
-                }
-            }
-
-            output.Save();
-            return true;
-        }
-
-        /// <summary>
-        /// Once the Parameter have been configured the Execute command can be called, it returns true if succesful
+         /// <summary>
+        /// Once the parameters have been configured, the Execute command can be called, it returns true if succesful
         /// </summary>
         public override bool Execute(ICancelProgressHandler cancelProgressHandler)
         {
-            IFeatureSet input = _inputParam[0].Value as IFeatureSet;
+            //Get the needed input and output parameters
+            IFeatureSet inputFeatures = _inputParam[0].Value as IFeatureSet;
             DoubleParam dp = _inputParam[1] as DoubleParam;
             double bufferDistance = 1;
             if (dp != null)
             {
                 bufferDistance = dp.Value;
             }
-
-            IFeatureSet output = _outputParam[0].Value as IFeatureSet;
-
-            return Execute(input, bufferDistance, output, cancelProgressHandler);
+            IFeatureSet outputFeatures = _outputParam[0].Value as IFeatureSet;
+            return GetBuffer(inputFeatures, bufferDistance, outputFeatures, cancelProgressHandler);
         }
 
+        
         /// <summary>
-        /// Executes the Buffer tool programaticaly
-        /// </summary>
-        /// <param name="input">The input polygon feature set</param>
-        /// <param name="bufferDistance">The included radius from current features to use when Buffering</param>
-        /// <param name="output">The output polygon feature set</param>
-        /// <returns></returns>
-        public bool Execute(IFeatureSet input, double bufferDistance, IFeatureSet output)
-        {
-            // Validates the input and output data
-            if (input == null || output == null)
-            {
-                return false;
-            }
-
-            if (input.FeatureType == FeatureType.Point)
-            {
-                return GetBuffer(input, bufferDistance, output);
-            }
-
-            if (input.FeatureType == FeatureType.MultiPoint)
-            {
-                return GetBuffer(input, bufferDistance, output);
-            }
-
-            if (input.FeatureType == FeatureType.Line)
-            {
-                return GetBuffer(input, bufferDistance, output);
-            }
-
-            if (input.FeatureType == FeatureType.Polygon)
-            {
-                return GetBuffer(input, bufferDistance, output);
-            }
-
-            return false;
-        }
-
-        // Ping Yang Added it for external testing 01/10
-
-        /// <summary>
-        /// Executes the buffer calculation
-        /// </summary>
-        /// <param name="input">The IFeatureSet</param>
-        /// <param name="bufferDistance">The double buffer distance</param>
-        /// <param name="output">The IFeatureSet output</param>
-        /// <param name="cancelProgressHandler">The progress handler</param>
-        /// <returns>True if the method was successful</returns>
-        public bool Execute(
-            IFeatureSet input, double bufferDistance, IFeatureSet output, ICancelProgressHandler cancelProgressHandler)
-        {
-            // Validates the input and output data
-            if (input == null || output == null)
-            {
-                return false;
-            }
-
-            if (input.FeatureType == FeatureType.Point)
-            {
-                return GetBuffer(input, bufferDistance, output, cancelProgressHandler);
-            }
-
-            if (input.FeatureType == FeatureType.MultiPoint)
-            {
-                return GetBuffer(input, bufferDistance, output, cancelProgressHandler);
-            }
-
-            if (input.FeatureType == FeatureType.Line)
-            {
-                return GetBuffer(input, bufferDistance, output, cancelProgressHandler);
-            }
-
-            if (input.FeatureType == FeatureType.Polygon)
-            {
-                return GetBuffer(input, bufferDistance, output, cancelProgressHandler);
-            }
-
-            return false;
-        }
-
-        /// <summary>
-        /// Get Buffer test method
+        /// Private function to compute the buffer and return the result to the Execute function.
         /// </summary>
         /// <param name="input"></param>
         /// <param name="bufferDistance"></param>
-        /// <param name="output"></param>
+        /// <param name="outputFeatures"></param>
         /// <returns></returns>
-        public bool GetBuffer(IFeatureSet input, double bufferDistance, IFeatureSet output)
+        private bool GetBuffer(IFeatureSet inputFeatures, double bufferDistance, IFeatureSet outputFeatures, ICancelProgressHandler cancelProgressHandler)
         {
-            int previous = 0;
-            int maxNo = input.Features.Count;
-            for (int i = 0; i < maxNo; i++)
+            int numFeatures = inputFeatures.Features.Count;
+            for (int i = 0; i < numFeatures; i++)
             {
-                input.Features[i].Buffer(bufferDistance, output);
-
+                inputFeatures.Features[i].Buffer(bufferDistance, outputFeatures);
                 // Here we update the progress
-                int current = Convert.ToInt32(i * 100 / maxNo);
-                if (current > previous)
-                {
-                    previous = current;
-                }
+                int progress = Convert.ToInt32(i * 100 / numFeatures);
+                cancelProgressHandler.Progress("buffer_tool", progress, "Buffering features.");
             }
-
-            output.Save();
             return true;
         }
-
-        /// <summary>
-        /// The Parameter array should be populated with default values here
-        /// </summary>
-        public override void Initialize()
-        {
-            _inputParam = new Parameter[2];
-            _inputParam[0] = new FeatureSetParam(TextStrings.InputFeatureSet);
-            _inputParam[1] = new DoubleParam(TextStrings.BufferDistance, 10.0);
-
-            // _inputParam[1].Value = 10.0;
-            _outputParam = new Parameter[1];
-            _outputParam[0] = new PolygonFeatureSetParam(TextStrings.OutputPolygonFeatureSet);
-        }
-
-        #endregion
     }
 }
