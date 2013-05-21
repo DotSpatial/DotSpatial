05d27bbd7448be56cc3697c9754d44c1e660b36e
danames_cp <SND\danames_cp>
2013-05-21 13:40:56 -0700

    Small change to controls description.

--- DotSpatial.Controls/Properties/AssemblyInfo.cs
+++ DotSpatial.Controls/Properties/AssemblyInfo.cs
@@ -1,43 +1,31 @@
-﻿// ********************************************************************************************************
-// Product Name: DotSpatial.Controls.dll
-// Description:  The Windows Forms user interface controls like the map, legend, toolbox, ribbon and others.
-// ********************************************************************************************************
-// The contents of this file are subject to the MIT License (MIT)
-// you may not use this file except in compliance with the License. You may obtain a copy of the License at
-// http://dotspatial.codeplex.com/license
-//
-// Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
-// ANY KIND, either expressed or implied. See the License for the specific language governing rights and
-// limitations under the License.
-//
-// The Original Code is from MapWindow.dll version 6.0
-//
-// The Initial Developer of this Original Code is Ted Dunsford. Created during refactoring 2010.
-// ********************************************************************************************************
+// *******************************************************************************************************
+// File: C:\dev\DotSpatial\DotSpatial.Controls\Properties\AssemblyInfo.cs
+// Description: Contains assembly information for this assembly 
+// Copyright & License: See www.DotSpatial.org.
+// *******************************************************************************************************
+// Contributor(s): Open source contributors may list themselves and their modifications here.
+// Contribution of code constitutes transferral of copyright to the DotSpatial Team.
+// *******************************************************************************************************
+
+ 
 
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("DotSpatial.Controls")]
-[assembly: AssemblyDescription("Windows Forms controls, including a map control.")]
+[assembly: AssemblyDescription("Windows Forms controls, includes a map control and legend control.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DotSpatial Team")]
 [assembly: AssemblyProduct("DotSpatial.Controls")]
-[assembly: AssemblyCopyright("Copyright © DotSpatial Team 2010-2012")]
+[assembly: AssemblyCopyright("Copyright © DotSpatial Team 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("86a8a37c-da91-46b5-ac7e-f559d5763011")]
 
-[assembly: AssemblyVersion("0.11.9.6")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5")]
+[assembly: AssemblyInformationalVersion("1.5 Experimental Release")]
+[assembly: AssemblyFileVersion("1.5.1.0")]
