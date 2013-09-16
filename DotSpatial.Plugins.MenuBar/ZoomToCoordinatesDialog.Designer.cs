151717971d254ee1608a8c6f2eae95f88790e688
jacobean_cp <SND\jacobean_cp>
2013-09-16 15:44:40 -0700

    Set the accept button on the Zoom to Coordinates form.

--- DotSpatial.Plugins.MenuBar/ZoomToCoordinatesDialog.Designer.cs
+++ DotSpatial.Plugins.MenuBar/ZoomToCoordinatesDialog.Designer.cs
@@ -140,6 +140,7 @@
             // 
             // ZoomToCoordinatesDialog
             // 
+            this.AcceptButton = this.AcceptButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(277, 172);
