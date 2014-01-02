675ad421fb841b5965123d6a499974d979a758e6
mogikanin_cp <SND\mogikanin_cp>
2014-01-02 04:08:33 -0800

    Fixed #24339: Label spatialite layer

--- Trunk/DotSpatial.Plugins.SpatiaLite/SpatiaLiteHelper.cs
+++ Trunk/DotSpatial.Plugins.SpatiaLite/SpatiaLiteHelper.cs
@@ -26,9 +26,9 @@ namespace DotSpatial.Plugins.SpatiaLite
         {
             try
             {
-                string pathVar = Environment.GetEnvironmentVariable("path", EnvironmentVariableTarget.User);
-                string sqLitePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                bool pathVariableExists = false;
+                var pathVar = Environment.GetEnvironmentVariable("path", EnvironmentVariableTarget.User);
+                var sqLitePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                var pathVariableExists = false;
                 if (!String.IsNullOrEmpty(pathVar))
                 {
                     if (pathVar.ToLower().Contains(sqLitePath.ToLower() + ";"))
@@ -54,13 +54,13 @@ namespace DotSpatial.Plugins.SpatiaLite
         /// </summary>
         public static bool CheckSpatiaLiteSchema(string connString)
         {
-            string qry = "SELECT name FROM sqlite_master WHERE type = 'table' AND name = 'geometry_columns'";
+            var qry = "SELECT name FROM sqlite_master WHERE type = 'table' AND name = 'geometry_columns'";
 
-            using (SQLiteCommand cmd = CreateCommand(connString, qry))
+            using (var cmd = CreateCommand(connString, qry))
             {
-                bool result = false;
+                var result = false;
                 cmd.Connection.Open();
-                object obj = cmd.ExecuteScalar();
+                var obj = cmd.ExecuteScalar();
                 if (obj != null)
                     result = true;
                 cmd.Connection.Close();
@@ -76,15 +76,15 @@ namespace DotSpatial.Plugins.SpatiaLite
         /// <returns>a list of all table names</returns>
         public List<string> GetTableNames(string connString)
         {
-            List<string> tableNameList = new List<string>();
+            var tableNameList = new List<string>();
 
-            using (SQLiteConnection cnn = new SQLiteConnection(connString))
+            using (var cnn = new SQLiteConnection(connString))
             {
-                string qry = "SELECT name FROM sqlite_master WHERE type = 'table'";
-                using (SQLiteCommand cmd = new SQLiteCommand(qry, cnn))
+                var qry = "SELECT name FROM sqlite_master WHERE type = 'table'";
+                using (var cmd = new SQLiteCommand(qry, cnn))
                 {
                     cmd.Connection.Open();
-                    SQLiteDataReader r = cmd.ExecuteReader();
+                    var r = cmd.ExecuteReader();
                     while (r.Read())
                     {
                         tableNameList.Add(r[0].ToString());
@@ -103,13 +103,13 @@ namespace DotSpatial.Plugins.SpatiaLite
         /// <returns>list of all column names</returns>
         public List<string> GetColumnNames(string connString, string tableName)
         {
-            List<string> columnNameList = new List<string>();
+            var columnNameList = new List<string>();
 
-            string qry = String.Format("PRAGMA table_info({0})", tableName);
-            using (SQLiteCommand cmd = CreateCommand(connString, qry))
+            var qry = String.Format("PRAGMA table_info({0})", tableName);
+            using (var cmd = CreateCommand(connString, qry))
             {
                 cmd.Connection.Open();
-                SQLiteDataReader r = cmd.ExecuteReader();
+                var r = cmd.ExecuteReader();
                 while (r.Read())
                 {
                     columnNameList.Add(r["name"].ToString());
@@ -126,21 +126,42 @@ namespace DotSpatial.Plugins.SpatiaLite
         /// <returns>the list of geometry columns</returns>
         public List<GeometryColumnInfo> GetGeometryColumns(string connString)
         {
-            List<GeometryColumnInfo> lst = new List<GeometryColumnInfo>();
-            string sql =
+            var lst = new List<GeometryColumnInfo>();
+            var sql =
             "SELECT f_table_name, f_geometry_column, type, coord_dimension, srid, spatial_index_enabled FROM geometry_columns";
-            using (SQLiteCommand cmd = CreateCommand(connString, sql))
+            using (var cmd = CreateCommand(connString, sql))
             {
                 cmd.Connection.Open();
 
-                SQLiteDataReader r = cmd.ExecuteReader();
+                var r = cmd.ExecuteReader();
                 while (r.Read())
                 {
-                    GeometryColumnInfo gci = new GeometryColumnInfo();
+                    var gci = new GeometryColumnInfo();
                     gci.TableName = Convert.ToString(r["f_table_name"]);
                     gci.GeometryColumnName = Convert.ToString(r["f_geometry_column"]);
                     gci.GeometryType = Convert.ToString(r["type"]);
-                    gci.CoordDimension = Convert.ToInt32(r["coord_dimension"]);
+                     try
+                    {
+                        gci.CoordDimension = Convert.ToInt32(r["coord_dimension"]);
+                    }
+                    catch (Exception)
+                    {
+                        var coords = Convert.ToString(r["coord_dimension"]);
+                        switch (coords)
+                        {
+                            case "XY":
+                                gci.CoordDimension = 2;
+                                break;
+                            case "XYZ":
+                            case "XYM":
+                                gci.CoordDimension = 3;
+                                break;
+                            case "XYZ;":
+                                gci.CoordDimension = 4;
+                                break;
+
+                        }
+                    }
                     gci.SRID = Convert.ToInt32(r["srid"]);
                     gci.SpatialIndexEnabled = false;
                     lst.Add(gci);
@@ -159,7 +180,7 @@ namespace DotSpatial.Plugins.SpatiaLite
         /// <returns>the resulting feature set</returns>
         public IFeatureSet ReadFeatureSet(string connString, GeometryColumnInfo featureSetInfo)
         {
-            string sql = String.Format("SELECT * FROM {0}", featureSetInfo.TableName);
+            var sql = String.Format("SELECT * FROM {0}", featureSetInfo.TableName);
             return ReadFeatureSet(connString, featureSetInfo, sql);
         }
 
@@ -172,34 +193,31 @@ namespace DotSpatial.Plugins.SpatiaLite
         /// <returns>the resulting feature set</returns>
         public IFeatureSet ReadFeatureSet(string connString, GeometryColumnInfo featureSetInfo, string sql)
         {
-            DataTable tab = new DataTable();
-            FeatureType fType = GetGeometryType(featureSetInfo.GeometryType);
-            FeatureSet fs = new FeatureSet(fType);
+            var fType = GetGeometryType(featureSetInfo.GeometryType);
+            FeatureSet fs = new SpatiaLiteFeatureSet(fType);
             fs.IndexMode = false; //setting the initial index mode..
 
-            using (SQLiteCommand cmd = CreateCommand(connString, sql))
+            using (var cmd = CreateCommand(connString, sql))
             {
                 cmd.Connection.Open();
 
                 RunInitialCommands(cmd.Connection);
 
                 //DotSpatial.Topology.Utilities.WkbReader wkbr = new DotSpatial.Topology.Utilities.WkbReader();
-                SpatiaLiteWkbReader wkbr = new SpatiaLiteWkbReader();
+                var wkbr = new SpatiaLiteWkbReader();
 
-                SQLiteDataReader rdr = cmd.ExecuteReader();
+                var rdr = cmd.ExecuteReader();
 
-                string[] columnNames = PopulateTableSchema(fs, featureSetInfo.GeometryColumnName, rdr);
-                int numColumns = fs.DataTable.Columns.Count;
-
+                var columnNames = PopulateTableSchema(fs, featureSetInfo.GeometryColumnName, rdr);
                 while (rdr.Read())
                 {
-                    byte[] wkb = rdr[featureSetInfo.GeometryColumnName] as byte[];
-                    IGeometry geom = wkbr.Read(wkb);
+                    var wkb = rdr[featureSetInfo.GeometryColumnName] as byte[];
+                    var geom = wkbr.Read(wkb);
 
-                    IFeature newFeature = fs.AddFeature(geom);
+                    var newFeature = fs.AddFeature(geom);
 
                     //populate the attributes
-                    foreach (string colName in columnNames)
+                    foreach (var colName in columnNames)
                     {
                         newFeature.DataRow[colName] = rdr[colName];
                     }
@@ -211,7 +229,7 @@ namespace DotSpatial.Plugins.SpatiaLite
                 fs.IndexMode = true;
 
                 //assign projection
-                ProjectionInfo proj = ProjectionInfo.FromEpsgCode(featureSetInfo.SRID);
+                var proj = ProjectionInfo.FromEpsgCode(featureSetInfo.SRID);
                 fs.Projection = proj;
 
                 return fs;
@@ -229,7 +247,7 @@ namespace DotSpatial.Plugins.SpatiaLite
             //RunInitialCommands(connString);
 
             //Find the geometry type and geometry column
-            GeometryColumnInfo geomInfo = FindGeometryColumnInfo(connString, sqlQuery);
+            var geomInfo = FindGeometryColumnInfo(connString, sqlQuery);
             return ReadFeatureSet(connString, geomInfo, sqlQuery);
         }
 
@@ -239,9 +257,9 @@ namespace DotSpatial.Plugins.SpatiaLite
 
             //string cmdText = "SELECT load_extension('E:/dev/DotSpatial/Debug/x86/Plugins/SpatiaLite/libspatialite-2.dll');";
 
-            string cmdText = "SELECT load_extension('libspatialite-2.dll')";
+            var cmdText = "SELECT load_extension('libspatialite-2.dll')";
 
-            using (SQLiteCommand cmd = new SQLiteCommand(cmdText, connection))
+            using (var cmd = new SQLiteCommand(cmdText, connection))
             {
                 try
                 {
@@ -255,42 +273,26 @@ namespace DotSpatial.Plugins.SpatiaLite
             }
         }
 
-        /// <summary>
-        /// Save a feature set to a SQLite database. The FeatureSet is saved to a table
-        /// with the same name as FeatureSet.Name
-        /// </summary>
-        /// <param name="connstring">database connection string</param>
-        /// <param name="fs">the feature set to save</param>
-        public void SaveFeatureSet(string connstring, IFeatureSet fs) { }
-
-        /// <summary>
-        /// Creates a new SpatiaLite database
-        /// </summary>
-        /// <param name="dbFileName">the database file name</param>
-        public void CreateNewDatabase(string dbFileName) { }
-
-        public IFeatureSet RunQuery(string connString, string query) { return new FeatureSet(); }
-
         //finds out the geometry column information..
         private GeometryColumnInfo FindGeometryColumnInfo(string connString, string sqlQuery)
         {
             GeometryColumnInfo result = null;
 
-            using (SQLiteCommand cmd = CreateCommand(connString, sqlQuery))
+            using (var cmd = CreateCommand(connString, sqlQuery))
             {
                 cmd.Connection.Open();
 
                 RunInitialCommands(cmd.Connection);
 
-                SpatiaLiteWkbReader wkbr = new SpatiaLiteWkbReader();
+                var wkbr = new SpatiaLiteWkbReader();
 
-                SQLiteDataReader rdr = cmd.ExecuteReader(CommandBehavior.SingleRow);
+                var rdr = cmd.ExecuteReader(CommandBehavior.SingleRow);
 
-                DataTable schemaTable = rdr.GetSchemaTable();
+                var schemaTable = rdr.GetSchemaTable();
                 foreach (DataRow r in schemaTable.Rows)
                 {
-                    string colName = Convert.ToString(r["ColumnName"]);
-                    string colDataType = Convert.ToString(r["DataType"]);
+                    var colName = Convert.ToString(r["ColumnName"]);
+                    var colDataType = Convert.ToString(r["DataType"]);
                     //if BLOB, then assume geometry column
                     if (Type.GetType(colDataType) == typeof(byte[]))
                     {
@@ -303,8 +305,8 @@ namespace DotSpatial.Plugins.SpatiaLite
                 if (result != null && rdr.HasRows)
                 {
                     rdr.Read();
-                    byte[] blob = rdr[result.GeometryColumnName] as byte[];
-                    IGeometry geom = wkbr.Read(blob);
+                    var blob = rdr[result.GeometryColumnName] as byte[];
+                    var geom = wkbr.Read(blob);
                     result.GeometryType = geom.GeometryType;
                 }
 
@@ -315,30 +317,21 @@ namespace DotSpatial.Plugins.SpatiaLite
 
         private string[] PopulateTableSchema(IFeatureSet fs, string geomColumnName, SQLiteDataReader rdr)
         {
-            DataTable schemaTable = rdr.GetSchemaTable();
-            List<string> columnNameList = new List<string>();
+            var schemaTable = rdr.GetSchemaTable();
+            var columnNameList = new List<string>();
             foreach (DataRow r in schemaTable.Rows)
             {
                 if (r["ColumnName"].ToString() != geomColumnName)
                 {
-                    string colName = Convert.ToString(r["ColumnName"]);
-                    string colDataType = Convert.ToString(r["DataType"]);
+                    var colName = Convert.ToString(r["ColumnName"]);
+                    var colDataType = Convert.ToString(r["DataType"]);
                     fs.DataTable.Columns.Add(colName, Type.GetType(colDataType));
                     columnNameList.Add(colName);
                 }
             }
             return columnNameList.ToArray();
         }
-
-        /// <summary>
-        /// Converts the SpatiaLite BLOB to a valid geometry
-        /// </summary>
-        /// <param name="blob">the SQLITE BLOB object</param>
-        /// <returns>a geometry object</returns>
-        private IGeometry ReadGeometry(byte[] blob)
-        {
-            return ReadPoint(blob);
-        }
+       
 
         private Point ReadPoint(byte[] blob)
         {
@@ -351,16 +344,16 @@ namespace DotSpatial.Plugins.SpatiaLite
                 try
                 {
                     reader = (byteOrder == ByteOrder.BigEndian) ? new BeBinaryReader(stream) : new BinaryReader(stream);
-                    int srid = reader.ReadInt32();
-                    double mbrMinX = reader.ReadDouble();
-                    double mbrMinY = reader.ReadDouble();
-                    double mbrMaxX = reader.ReadDouble();
-                    double mbrMaxY = reader.ReadDouble();
-                    byte mbrEnd = reader.ReadByte();
-                    int geomType = reader.ReadInt32();
-                    double ptX = reader.ReadDouble();
-                    double ptY = reader.ReadDouble();
-                    byte geomEnd = reader.ReadByte();
+                    var srid = reader.ReadInt32();
+                    var mbrMinX = reader.ReadDouble();
+                    var mbrMinY = reader.ReadDouble();
+                    var mbrMaxX = reader.ReadDouble();
+                    var mbrMaxY = reader.ReadDouble();
+                    var mbrEnd = reader.ReadByte();
+                    var geomType = reader.ReadInt32();
+                    var ptX = reader.ReadDouble();
+                    var ptY = reader.ReadDouble();
+                    var geomEnd = reader.ReadByte();
                     return new Point(ptX, ptY);
                 }
                 finally
@@ -397,7 +390,7 @@ namespace DotSpatial.Plugins.SpatiaLite
         /// <returns>the SpatiaLite command object</returns>
         private static SQLiteCommand CreateCommand(string conString, string cmdText)
         {
-            SQLiteConnection con = new SQLiteConnection(conString);
+            var con = new SQLiteConnection(conString);
             return new SQLiteCommand(cmdText, con);
         }
     }
