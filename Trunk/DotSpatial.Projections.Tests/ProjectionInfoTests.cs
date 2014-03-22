ef8eaddfd4520f4750ba679cf93ee3760765fbf6
mogikanin_cp <SND\mogikanin_cp>
2014-03-22 03:17:17 -0700

    Added unit test which proves that Central_Parallel is correctly parsed from esri string. #22934

--- Trunk/DotSpatial.Projections.Tests/ProjectionInfoTests.cs
+++ Trunk/DotSpatial.Projections.Tests/ProjectionInfoTests.cs
@@ -143,5 +143,16 @@ namespace DotSpatial.Projections.Tests
             var actual = ProjectionInfo.FromProj4String(proj4str);
             Assert.AreEqual(target.Geoc, actual.Geoc);
         }
+
+        [Test]
+        public void EsriCentral_ParallelParse()
+        {
+            // Test for https://dotspatial.codeplex.com/workitem/22934
+            const string esriStr = "PROJCS[\"Albers Equal-Area Conic [SHG]\",GEOGCS[\"GCS_North_American_1983\",DATUM[\"D_North_American_1983\",SPHEROID[\"GRS_1980\",6378137.0,298.257222101]],PRIMEM[\";Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]],PROJECTION[\"Albers\"],PARAMETER[\"False_Easting\",0.0],PARAMETER[\"False_Northing\",0.0],PARAMETER[\"Central_Meridian\",-96.0],PARAMETER[\"Standard_Parallel_1\",29.5],PARAMETER[\"Standard_Parallel_2\",45.5],PARAMETER[\"Central_Parallel\",23.0],UNIT[\"Meter\",1.0]]";
+            var pi = ProjectionInfo.FromEsriString(esriStr);
+            Assert.AreEqual(23.0, pi.LatitudeOfOrigin);
+            var proj4Str = pi.ToProj4String();
+            Assert.IsTrue(proj4Str.Contains("+lat_0=23"));
+        }
     }
 }
