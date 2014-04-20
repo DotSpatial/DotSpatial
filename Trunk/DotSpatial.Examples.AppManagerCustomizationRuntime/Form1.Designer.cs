747f2da4f20a6bd2d75aeec05870f44edfbf6ca2
mogikanin_cp <SND\mogikanin_cp>
2014-04-20 03:55:08 -0700

    Re-add sample project

---
+++ Trunk/DotSpatial.Examples.AppManagerCustomizationRuntime/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace DotSpatial.Examples.AppManagerCustomizationRuntime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.appManager = new DotSpatial.Controls.AppManager();
+            this.map1 = new DotSpatial.Controls.Map();
+            this.SuspendLayout();
+            // 
+            // appManager
+            // 
+            this.appManager.CompositionContainer = null;
+            this.appManager.Directories = ((System.Collections.Generic.List<string>)(resources.GetObject("appManager.Directories")));
+            this.appManager.DockManager = null;
+            this.appManager.HeaderControl = null;
+            this.appManager.Legend = null;
+            this.appManager.Map = this.map1;
+            this.appManager.ProgressHandler = null;
+            this.appManager.ShowExtensionsDialog = DotSpatial.Controls.ShowExtensionsDialog.Default;
+            // 
+            // map1
+            // 
+            this.map1.AllowDrop = true;
+            this.map1.BackColor = System.Drawing.Color.White;
+            this.map1.CollectAfterDraw = false;
+            this.map1.CollisionDetection = false;
+            this.map1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.map1.ExtendBuffer = false;
+            this.map1.FunctionMode = DotSpatial.Controls.FunctionMode.None;
+            this.map1.IsBusy = false;
+            this.map1.IsZoomedToMaxExtent = false;
+            this.map1.Legend = null;
+            this.map1.Location = new System.Drawing.Point(0, 0);
+            this.map1.Name = "map1";
+            this.map1.ProgressHandler = null;
+            this.map1.ProjectionModeDefine = DotSpatial.Controls.ActionMode.Prompt;
+            this.map1.ProjectionModeReproject = DotSpatial.Controls.ActionMode.Prompt;
+            this.map1.RedrawLayersWhileResizing = false;
+            this.map1.SelectionEnabled = true;
+            this.map1.Size = new System.Drawing.Size(638, 387);
+            this.map1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 387);
+            this.Controls.Add(this.map1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Controls.AppManager appManager;
+        private Controls.Map map1;
+    }
+}
+
---
+++ Trunk/DotSpatial.Examples.AppManagerCustomizationRuntime/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Forms;
+
+namespace DotSpatial.Examples.AppManagerCustomizationRuntime
+{
+    public partial class Form1 : Form
+    {
+        [Export("Shell", typeof(ContainerControl))]
+        private static ContainerControl Shell;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (DesignMode) return;
+
+            //------ These 2 lines required to load extensions
+            Shell = this;
+            appManager.LoadExtensions();
+            //-----------
+
+            appManager.UpdateProgress("Ready"); // Show some status message
+        }
+
+    }
+}
