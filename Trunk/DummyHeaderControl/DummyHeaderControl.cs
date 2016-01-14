efe8827f2ccce62f80b7398e7b4bfcc3e72a6cc3
jany__cp <jany_cp@online.de>
2016-01-14 11:01:30 +0100

    Added DUmmyHeaderControl example for people who need an empty HeaderControl

---
+++ Trunk/DummyHeaderControl/DummyHeaderControl.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DotSpatial.Controls.Header;
+using System.ComponentModel.Composition;
+
+
+namespace DummyHeaderControl
+{
+    /// <summary>
+    /// This is an examplary empty HeaderControl.
+    /// </summary>
+    [Export(typeof(IHeaderControl))]
+    public class DummyHeaderControl : IHeaderControl
+    {
+        public DropDownActionItem MapServiceDropDown { get; private set; }
+        public void Add(HeaderItem item)
+        {
+            switch (item.Key)
+            {
+                case "kServiceDropDown":
+                    {
+                        MapServiceDropDown = item as DropDownActionItem;
+                    }
+                    break;
+            }
+        }
+
+        public void Remove(string key)
+        {
+        }
+
+        public void RemoveAll()
+        {
+        }
+
+        public void SelectRoot(string key)
+        {
+        }
+
+        public event EventHandler<RootItemEventArgs> RootItemSelected;
+    }
+}
