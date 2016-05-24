7e10d1a90899d93ca17a834b222de1f96f9f1497
mogikanin <mogikanin.tir@gmail.com>
2016-05-24 18:14:15 +0300

    Update DotSpatial.GeoApi to 1.7.4.2. This also closes #746

--- Source/DotSpatial.Data.Tests/GeometryTests.cs
+++ Source/DotSpatial.Data.Tests/GeometryTests.cs
@@ -36,7 +36,6 @@ namespace DotSpatial.Data.Tests
         }
 
         [Test(Description = @"https://github.com/DotSpatial/DotSpatial/issues/746")]
-        [Ignore] // todo: Unignore after updating GeoAPI
         public void OverlapWith2XyEqualCoordinatesInARow()
         {
             #region cList definitions
