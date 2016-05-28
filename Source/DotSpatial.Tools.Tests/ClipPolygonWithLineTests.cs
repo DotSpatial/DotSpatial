eb05d1ca1f7a61fc2d04ade4eec8a04cf4a898a9
jany-tenaj <jany_cp@online.de>
2016-05-28 17:21:50 +0200

    added test case for ClipPolygonWithLine error from commit 6b13fde

---
+++ Source/DotSpatial.Tools.Tests/ClipPolygonWithLineTests.cs
@@ -0,0 +1,28 @@
+﻿using DotSpatial.Data;
+using DotSpatial.Tests.Common;
+using NUnit.Framework;
+
+namespace DotSpatial.Tools.Tests
+{
+    [TestFixture]
+    public class ClipPolygonWithLineTests
+    {
+
+        [Test]
+        public void ExcecuteDoesntLoseOutputFilename()
+        {
+            var target = new ClipPolygonWithLine();
+
+            IFeatureSet fsInput1 = FeatureSet.Open(@"Data\ClipPolygonWithLineTests\polygon.shp");
+            IFeatureSet fsInput2 = FeatureSet.Open(@"Data\ClipPolygonWithLineTests\line.shp");
+            IFeatureSet fsOutput = new FeatureSet { Filename = FileTools.GetTempFileName(".shp") };
+
+            Assert.DoesNotThrow(() => target.Execute(fsInput1, fsInput2, fsOutput, new MockProgressHandler()));
+            Assert.AreNotEqual(string.IsNullOrWhiteSpace(fsOutput.Filename), true);
+            
+            FileTools.DeleteShapeFile(fsOutput.Filename);
+        }
+
+
+    }
+}
