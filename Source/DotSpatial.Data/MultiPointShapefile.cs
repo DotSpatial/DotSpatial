9ba531e44efa3663f4c3efdade6c2cc6990fd00f
mogikanin <mogikanin.tir@gmail.com>
2016-10-17 12:50:20 +0300

    Fix build error.

--- Source/DotSpatial.Data/MultiPointShapefile.cs
+++ Source/DotSpatial.Data/MultiPointShapefile.cs
@@ -17,7 +17,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Windows.Forms;
 using GeoAPI.Geometries;
 
 namespace DotSpatial.Data
