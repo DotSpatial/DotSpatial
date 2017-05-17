c7923654617592aeca0b803ecc16b16d8ca3cce5
jany-tenaj <jany_cp@online.de>
2017-05-17 16:01:42 +0200

    Updated changelog fixed positioning serialization error

---
+++ Source/DotSpatial.Positioning.Tests/SampleData1.cs
@@ -0,0 +1,181 @@
+// Copyright (c) DotSpatial Team. All rights reserved.
+// Licensed under the MIT license. See License.txt file in the project root for full license information.
+
+namespace DotSpatial.Positioning.Tests
+{
+    /// <summary>
+    /// This class contains all ISerializable classes
+    /// </summary>
+    public class SampleData1
+    {
+        /// <summary>
+        /// Gets or sets the position.
+        /// </summary>
+        public Position Position { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second position.
+        /// </summary>
+        public Position Position2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the distance.
+        /// </summary>
+        public Distance Distance { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second distance.
+        /// </summary>
+        public Distance Distance2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the angle.
+        /// </summary>
+        public Angle Angle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second angle.
+        /// </summary>
+        public Angle Angle2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the area.
+        /// </summary>
+        public Area Area { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second area.
+        /// </summary>
+        public Area Area2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the  azimuth.
+        /// </summary>
+        public Azimuth Azimuth { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second azimuth.
+        /// </summary>
+        public Azimuth Azimuth2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the cartesian point.
+        /// </summary>
+        public CartesianPoint CartesianPoint { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second cartesian point.
+        /// </summary>
+        public CartesianPoint CartesianPoint2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the elevation.
+        /// </summary>
+        public Elevation Elevation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second elevation.
+        /// </summary>
+        public Elevation Elevation2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ellipsoid.
+        /// </summary>
+        public Ellipsoid Ellipsoid { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second ellipsoid.
+        /// </summary>
+        public Ellipsoid Ellipsoid2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the geographic rectangle.
+        /// </summary>
+        public GeographicRectangle GeographicRectangle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second geographic rectangle.
+        /// </summary>
+        public GeographicRectangle GeographicRectangle2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the latitude.
+        /// </summary>
+        public Latitude Latitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second latitude.
+        /// </summary>
+        public Latitude Latitude2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the longitude.
+        /// </summary>
+        public Longitude Longitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second longitude.
+        /// </summary>
+        public Longitude Longitude2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the PointD.
+        /// </summary>
+        public PointD PointD { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second PointD.
+        /// </summary>
+        public PointD PointD2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Position3D.
+        /// </summary>
+        public Position3D Position3D { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second Position3D.
+        /// </summary>
+        public Position3D Position3D2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the radian.
+        /// </summary>
+        public Radian Radian { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second radian.
+        /// </summary>
+        public Radian Radian2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the SizeD.
+        /// </summary>
+        public SizeD SizeD { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second SizeD.
+        /// </summary>
+        public SizeD SizeD2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the speed.
+        /// </summary>
+        public Speed Speed { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second speed.
+        /// </summary>
+        public Speed Speed2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the velocity.
+        /// </summary>
+        public Velocity Velocity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second velocity.
+        /// </summary>
+        public Velocity Velocity2 { get; set; }
+    }
+}
