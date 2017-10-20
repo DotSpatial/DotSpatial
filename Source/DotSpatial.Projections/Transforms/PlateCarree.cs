97a8b73b71c7114dce3d1c889b077daf34352f1f
Christoph <christoph.perger@spatial-focus.net>
2017-10-20 16:55:43 +0200

    Added PlateCarree (EPSG:32662) as an alias of EquidistantCylindrical

---
+++ Source/DotSpatial.Projections/Transforms/PlateCarree.cs
@@ -0,0 +1,29 @@
+﻿// ********************************************************************************************************
+// Contributor(s): (Open source contributors should list themselves and their modifications here).
+//        Name         |    Date    |        Comment
+// --------------------|------------|------------------------------------------------------------
+// Christoph Perger    | 20/10/2017 |  Created Plate Carree as an alias of EquidistantCylindrical
+// ********************************************************************************************************
+
+namespace DotSpatial.Projections.Transforms
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class PlateCarree : EquidistantCylindrical
+	{
+		#region Constructors
+
+		/// <summary>
+		/// Creates a new instance of PlateCarree as an alias of EquidistantCylindrical
+		/// </summary>
+		public PlateCarree()
+		{
+			Proj4Name = "eqc";
+			Name = "Plate_Carree";
+		}
+
+		#endregion
+
+	}
+}
