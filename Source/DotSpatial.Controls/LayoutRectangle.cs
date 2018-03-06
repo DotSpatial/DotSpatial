88942a02f62297189eb67ab515e3b53ea2cabecd
Leo Calvis <leo.calvis@cgx-group.com>
2018-03-06 15:57:15 +0100

    r8764 : Ajout du dessin du Background pour les elements de type Rectangle dans le layout C4F

--- Source/DotSpatial.Controls/LayoutRectangle.cs
+++ Source/DotSpatial.Controls/LayoutRectangle.cs
@@ -1,8 +1,12 @@
 ﻿// Copyright (c) DotSpatial Team. All rights reserved.
 // Licensed under the MIT license. See License.txt file in the project root for full license information.
 
+using System.ComponentModel;
 using System.Drawing;
+using System.Drawing.Design;
+using System.Drawing.Drawing2D;
 using DotSpatial.Symbology;
+using DotSpatial.Symbology.Forms;
 
 namespace DotSpatial.Controls
 {
@@ -27,6 +31,21 @@ namespace DotSpatial.Controls
 
         #region Methods
 
+        
+        IPolygonSymbolizer _RectangleBackground = null;
+
+
+        [TypeConverter(typeof(GeneralTypeConverter)), Browsable(true), Category("Symbol"), Editor(typeof(PolygonSymbolizerEditor), typeof(UITypeEditor))]
+        public new IPolygonSymbolizer Background
+        {
+            get { return _RectangleBackground; }
+            set
+            {
+                _RectangleBackground = value; base.UpdateThumbnail(); base.OnInvalidate();
+
+            }
+        }
+        
         /// <summary>
         /// Doesn't need to do anything now because the drawing code is in the background property of the base class.
         /// </summary>
@@ -34,6 +53,21 @@ namespace DotSpatial.Controls
         /// <param name="printing">Indicates whether the content is printed or previewed.</param>
         public override void Draw(Graphics g, bool printing)
         {
+                        if (Background != null)
+            {
+                GraphicsPath gp = new GraphicsPath();
+                Pen myPen = new Pen(Background.OutlineSymbolizer.GetFillColor(), (int)Background.GetOutlineWidth());
+                RectangleF[] rectangles = { this.Rectangle };
+                g.DrawRectangles(myPen, rectangles);
+                gp.AddRectangle(this.Rectangle);
+                SolidBrush Brush = new SolidBrush(Background.GetFillColor());
+
+                g.FillPath(Brush, gp);
+
+                Brush.Dispose();
+                myPen.Dispose();
+
+            }
         }
 
         #endregion
