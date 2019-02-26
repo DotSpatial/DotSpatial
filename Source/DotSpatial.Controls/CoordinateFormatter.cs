04c562014eea077b581fb1a7d4c05695789839f8
Leo Calvis <leo.calvis@cgx-group.com>
2019-02-26 14:27:55 +0100

    check string for CoordinateFormatter

--- Source/DotSpatial.Controls/CoordinateFormatter.cs
+++ Source/DotSpatial.Controls/CoordinateFormatter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using DotSpatial.Serialization;
 using GeoAPI.Geometries;
 
@@ -8,12 +9,24 @@ namespace DotSpatial.Controls
     {
         public override object FromString(string value)
         {
-            var xyzm = value.Split(',');
-            double x = parse(xyzm[0]);
-            double y = parse(xyzm[1]);
-            double z = parse(xyzm[2]);
-            double m = parse(xyzm[3]);
-            return new Coordinate(x, y, z, m);
+            value = value.Trim();
+
+            //Something like "(12.5, 2.0, NaN, NaN)"
+            Regex rx = new Regex(@"^\((\s*(([0-9]+(\.+[0-9]+)?)|(NaN))\s*,){3}(\s*(([0-9]+(\.+[0-9]+)?)|(NaN)))\s*\)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            if (rx.IsMatch(value))
+            {
+                var xyzm = value.Split(',');
+                double x = parse(xyzm[0]);
+                double y = parse(xyzm[1]);
+                double z = parse(xyzm[2]);
+                double m = parse(xyzm[3]);
+                return new Coordinate(x, y, z, m);
+            }
+            else
+            {
+                throw new FormatException(value + "is not a coordinate");
+            }
         }
 
         private double parse(string val)
