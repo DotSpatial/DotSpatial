be772960f129c059eb388d8dd2b620fe7cfb4598
Abel G. Perez <sindizzy@gmail.com>
2022-11-26 14:53:43 -0600

    Update CoordinateSystemTypeTest.cs
    added a test for Alaska Zone 1

--- Source/Tests/DotSpatial.Projections.Tests/CoordinateSystemTypeTest.cs
+++ Source/Tests/DotSpatial.Projections.Tests/CoordinateSystemTypeTest.cs
@@ -37,6 +37,7 @@ namespace DotSpatial.Projections.Tests
             ProjectionInfo prj = ProjectionInfo.FromEsriString("PROJCS[\"Sphere_Vertical_Perspective\",GEOGCS[\"GCS_WGS_1984\",DATUM[\"D_WGS_1984\",SPHEROID[\"WGS_1984\",6378137,298.2572235629972]],PRIMEM[\"Greenwich\",0],UNIT[\"Degree\",0.017453292519943295]], PARAMETER[\"Scale_Factor\",1],UNIT[\"Meter\",1]]");
             DebugProjection("CustMaj", "CustMin", prj);
 
+            //this will throw an exception
             //prj = ProjectionInfo.FromEsriString("PROJCS[\"Sphere_Vertical_Perspective\",GEOGCS[\"GCS_WGS_1984\",DATUM[\"D_WGS_1984\",SPHEROID[\"WGS_1984\",6378137.0,298.257223563]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]],PROJECTION[\"Vertical_Near_Side_Perspective\"],PARAMETER[\"False_Easting\",0.0],PARAMETER[\"False_Northing\",0.0],PARAMETER[\"Longitude_Of_Center\",0.0],PARAMETER[\"Latitude_Of_Center\",0.0],PARAMETER[\"Height\",35800000.0],UNIT[\"Meter\",1.0]]");
             //DebugProjection("CustMaj", "CustMin", prj);
 
@@ -45,6 +46,96 @@ namespace DotSpatial.Projections.Tests
 
             prj = ProjectionInfo.FromProj4String("+proj=wink1 +lon_0=0 +lat_ts=50.4597762521898 +x_0=0 +y_0=0 +R=6371000 +units=m +no_defs +type=crs");
             DebugProjection("CustMaj", "CustMin", prj);
+        }
+
+        /// <summary>
+        /// Alaska Zone 1 reprojection test
+        /// </summary>
+        [Test]
+        public void AlaskaZone1Test()
+        {
+            try
+            {
+                //**********************************************************************
+                //built in projection NAD1927StatePlaneAlaska1FIPS5001
+                //**********************************************************************
+                ProjectionInfo projSourceA = KnownCoordinateSystems.Projected.StatePlaneNad1927.NAD1927StatePlaneAlaska1FIPS5001;
+                //ProjectionInfo projSourceA = ProjectionInfo.FromProj4String("+proj=omerc +lat_0=57 +lonc=-133.6666666666667 +alpha=-36.86989764583333 +k=0.9999 +x_0=5000000.000000102 +y_0=-5000000.000000102 +ellps=clrk66 +datum=NAD27 +to_meter=0.3048006096012192 +no_defs");
+                ProjectionInfo projTarget = KnownCoordinateSystems.Geographic.NorthAmerica.NorthAmericanDatum1927;
+                System.Diagnostics.Debug.Print("source:{0}", projSourceA.ToProj4String());
+                System.Diagnostics.Debug.Print("target:{0}", projTarget.ToProj4String());
+
+                //setup source coordinates
+                double Xsource = 0;
+                double Ysource = 0;
+                double Zsource = 0;
+                double[] xy = { Xsource, Ysource };
+                double[] z = { Zsource };
+
+                //reproject the coordinates from the source projection to target projection
+                Reproject.ReprojectPoints(xy, z, projSourceA, projTarget, 0, 1);
+
+                //assign reprojected coordinates from the result to the target
+                double Xtarget = xy[0];
+                double Ytarget = xy[1];
+                double Ztarget = z[0];
+
+                //build a string
+                StringBuilder sb = new();
+                sb.AppendFormat("Source\r\n  InputString: {0}\r\n  EsriPrj: {1}\r\n  Proj4: {2}\r\n", projSourceA.CoordinateSystemInputString, projSourceA.ToEsriString(), projSourceA.ToProj4String());
+                sb.AppendFormat("  IsLatLon: {0}\r\n  CoordinateSystemType={1}\r\n  Transform={2}\r\n  Datum={3}\r\n", projSourceA.IsLatLon, projSourceA.CoordinateSystemType, projSourceA.Transform?.Name, projSourceA.GeographicInfo?.Datum?.Name);
+
+                sb.AppendFormat("Target\r\n  InputString: {0}\r\n  EsriPrj: {1}\r\n  Proj4: {2}\r\n", projTarget.CoordinateSystemInputString, projTarget.ToEsriString(), projTarget.ToProj4String());
+                sb.AppendFormat("  IsLatLon: {0}\r\n  CoordinateSystemType={1}\r\n  Transform={2}\r\n  Datum={3}\r\n", projTarget.IsLatLon, projTarget.CoordinateSystemType, projTarget.Transform?.Name, projTarget.GeographicInfo?.Datum?.Name);
+
+                sb.AppendLine("           Source         Target");
+                sb.AppendFormat("  X    {0,10:N4}     {1,25:N4}\r\n", Xsource, Xtarget);
+                sb.AppendFormat("  Y    {0,10:N4}     {1,25:N4}\r\n", Ysource, Ytarget);
+                sb.AppendFormat("  Z    {0,10:N4}     {1,25:N4}", Zsource, Ztarget);
+
+                //print the string
+                System.Diagnostics.Debug.Print(sb.ToString());
+
+                //**********************************************************************
+                //custome projection for NAD1927 State Plane Alaska 1
+                //**********************************************************************
+                ProjectionInfo projSourceB = ProjectionInfo.FromProj4String("+proj=omerc +lat_0=57 +lonc=-133.666666666667  +alpha=323.130102361111 +gamma=323.130102361111 +k=0.9999 +x_0=5000000.001016 +y_0=-5000000.001016 +ellps=clrk66 +nadgrids=NTv2_0.gsb +units=us-ft +no_defs +type=crs");
+
+                //setup source coordinates
+                Xsource = 0;
+                Ysource = 0;
+                Zsource = 0;
+                xy = new double[] { Xsource, Ysource };
+                z = new double[] { Zsource };
+
+                //reproject the coordinates from the source projection to target projection
+                Reproject.ReprojectPoints(xy, z, projSourceB, projTarget, 0, 1);
+
+                //assign reprojected coordinates from the result to the target
+                Xtarget = xy[0];
+                Ytarget = xy[1];
+                Ztarget = z[0];
+
+                //build a string
+                sb.Clear();
+                sb.AppendFormat("Source\r\n  InputString: {0}\r\n  EsriPrj: {1}\r\n  Proj4: {2}\r\n", projSourceB.CoordinateSystemInputString, projSourceB.ToEsriString(), projSourceB.ToProj4String());
+                sb.AppendFormat("  IsLatLon: {0}\r\n  CoordinateSystemType={1}\r\n  Transform={2}\r\n  Datum={3}\r\n", projSourceB.IsLatLon, projSourceB.CoordinateSystemType, projSourceB.Transform?.Name, projSourceB.GeographicInfo?.Datum?.Name);
+
+                sb.AppendFormat("Target\r\n  InputString: {0}\r\n  EsriPrj: {1}\r\n  Proj4: {2}\r\n", projTarget.CoordinateSystemInputString, projTarget.ToEsriString(), projTarget.ToProj4String());
+                sb.AppendFormat("  IsLatLon: {0}\r\n  CoordinateSystemType={1}\r\n  Transform={2}\r\n  Datum={3}\r\n", projTarget.IsLatLon, projTarget.CoordinateSystemType, projTarget.Transform?.Name, projTarget.GeographicInfo?.Datum?.Name);
+
+                sb.AppendLine("           Source         Target");
+                sb.AppendFormat("  X    {0,10:N4}     {1,25:N4}\r\n", Xsource, Xtarget);
+                sb.AppendFormat("  Y    {0,10:N4}     {1,25:N4}\r\n", Ysource, Ytarget);
+                sb.AppendFormat("  Z    {0,10:N4}     {1,25:N4}", Zsource, Ztarget);
+
+                //print the string
+                System.Diagnostics.Debug.Print(sb.ToString());
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print(ex.ToString());
+            }
         }
 
         private void TestCategoryHolder(ICoordinateSystemCategoryHolder CoordSysCategoryHolder)
@@ -86,7 +177,7 @@ namespace DotSpatial.Projections.Tests
                 double Ytarget = xy[1];
                 double Ztarget = z[0];
 
-                //build a stgring
+                //build a string
                 StringBuilder sb = new();
                 sb.AppendLine("    Source(WGS84)                  Target(this)");
                 sb.AppendFormat("  X    {0,10:N4}     {1,25:N4}\r\n", Xsource, Xtarget);
@@ -96,7 +187,7 @@ namespace DotSpatial.Projections.Tests
                 //return the string
                 return sb.ToString();
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 return "  <reproject.error>";
             }
