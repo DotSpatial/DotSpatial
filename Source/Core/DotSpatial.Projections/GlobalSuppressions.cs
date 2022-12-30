920c279397d8bc25c92390427b52deb669096d24
jany-tenaj <jany-tenaj@users.noreply.github.com>
2022-12-30 15:35:25 +0100

    switched output path to bin\Configuration because TargetFramework is no longer needed because we're back to one Framework

---
+++ Source/Core/DotSpatial.Projections/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) DotSpatial Team. All rights reserved.
+// Licensed under the MIT, license. See License.txt file in the project root for full license information.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0056:Indexoperator verwenden", Justification = "Doesn't work for netstandard2.0", Scope = "namespaceanddescendants", Target = "~N:DotSpatial.Projections")]
+[assembly: SuppressMessage("Style", "IDE0057:Bereichsoperator verwenden", Justification = "Doesn't work for netstandard2.0", Scope = "namespaceanddescendants", Target = "~N:DotSpatial.Projections")]
